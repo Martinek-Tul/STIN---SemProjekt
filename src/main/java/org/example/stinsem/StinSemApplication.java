6587c4ddc0628ed8b6edf07217e2f3635e3e103c
Matous-TUL <matymartinek@seznam.cz>
2026-03-26 17:32:48 +0100

    Inicial commit

---
+++ src/main/java/org/example/stinsem/StinSemApplication.java
@@ -0,0 +1,13 @@
+package org.example.stinsem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StinSemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StinSemApplication.class, args);
+    }
+
+}
