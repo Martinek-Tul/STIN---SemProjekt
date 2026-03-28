ca6f3f7a5b1b501eea002c547938ef0da3524a14
Matous-TUL <matymartinek@seznam.cz>
2026-03-28 21:42:09 +0100

    UserSettings added

---
+++ src/main/java/model/UserSettings.java
@@ -0,0 +1,30 @@
+package model;
+
+import lombok.Getter;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class UserSettings {
+    @Getter
+    private final String baseCurrency;
+    @Getter
+    private final List<String> selectedCurrencies;
+
+    private static final List<String> availableCurrencies = List.of(
+            "AUD", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP",
+            "HKD", "HUF", "ILS", "JPY", "MXN", "NOK", "NZD", "PHP",
+            "PLN", "RON", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"
+    );
+    public UserSettings(String baseCurrency, List<String> selectedCurrencies){
+        if(!availableCurrencies.contains(baseCurrency)){
+            throw new IllegalArgumentException("Nepodporovaná měna: "+baseCurrency);
+        }
+        if(!new HashSet<>(availableCurrencies).containsAll(selectedCurrencies)){
+            throw new IllegalArgumentException("Seznam obsahuje nepodporovanou měnu");
+        }
+        this.baseCurrency = baseCurrency;
+        this.selectedCurrencies = selectedCurrencies;
+    }
+
+}
--- src/main/java/org/example/stinsem/StinSemApplication.java
+++ src/main/java/org/example/stinsem/StinSemApplication.java
@@ -3,11 +3,14 @@ package org.example.stinsem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication()
 public class StinSemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StinSemApplication.class, args);
+
+
     }
 
 }
